4ba5f4263f2992a3582137b40bec9cc6538e69a5
Justin Barnikow <justinsbarnikow@gmail.com>
2022-02-20 15:20:43 -0500

    fix ReciPod typo

--- src/Help.js
+++ src/Help.js
@@ -6,7 +6,7 @@ function Help() {
         <div className='help_page grid_item'>
             Hello! Welcome to Recipod, an app created to make life easier for you. 
             <br /> <br />
-            ReciPod works by curating a list of podcasts equal in length to the recipe you choose.
+            Recipod works by curating a list of podcasts equal in length to the recipe you choose.
             <br /> <br />
             Just search, select, and get to work!
         </div>
